219beea6c7c82659c2322491e8237d8c6460d80c
faziiQ <fm013361@gmail.com>
2025-05-22 01:53:02 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()],
+    react()
+  ],
+  build: {
+    rollupOptions: {
+      external: ['react-hot-toast']
+    }
+  }
 })
