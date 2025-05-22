a49b84226e5cab84067acbd32b4218a6e262cbe2
faziiQ <fm013361@gmail.com>
2025-05-22 15:08:59 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,25 @@
-
-import tailwindcss from '@tailwindcss/vite'
-
-
-// https://vite.dev/config/
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
+  plugins: [
+    tailwindcss(),
+    react()],
+    build: {
     rollupOptions: {
       external: [
-        'react-icons/md',  // Add this
-        'react-icons/im'   // Add if using other icon sets
+        'react-icons/md',
+        'react-icons/im',
+         'react-icons/fa'  
+
       ]
     }
+  },
+   optimizeDeps: {
+    include: ['react-icons/md', 'react-icons/im']
   }
 })
+
+
