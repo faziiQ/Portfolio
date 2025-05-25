cc56f1794845445dbfe91d8c5da9391204ccec50
faziiQ <fm013361@gmail.com>
2025-05-25 15:28:14 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import NavBar from './Components/NavBar'
+ import Portfolio from './Components/Portfolio'
+import Experience from './Components/Experience'
 import Home from './Components/Home'
 import About from './Components/About'
-import Portfolio from './Components/Portfolio'
-import Experience from './Components/Experience'
+import Navbar from './Components/NavBar'
 import Contact from './Components/Contact'
 import Footer from './Components/Footer'
 import { Toaster } from 'react-hot-toast'
@@ -10,16 +10,18 @@ import { Toaster } from 'react-hot-toast'
 function App() {
   return (
     <>
-      <NavBar />
-      <div className="pt-10">
+      <div>
+        <Navbar />
+      <div className="pt-10"> {/* Increased padding to ensure content doesn't hide under navbar */}
         <div id="Home"><Home /></div>
         <div id="About"><About /></div>
         <div id="Projects"><Portfolio /></div>
         <div id="Experience"><Experience /></div>
         <div id="Contact"><Contact /></div>
+        <Footer />
       </div>
-      <Footer />
-      <Toaster />
+      <Toaster  position="top-center" />
+      </div>
     </>
   )
 }
