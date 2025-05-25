27cce029cc42d13e744486828111669750c350f8
faziiQ <fm013361@gmail.com>
2025-05-25 15:59:39 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,27 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()
-  ],
-  // Remove external completely unless you have specific reason to exclude something
-  css: {
-    postcss: './postcss.config.js',
-    modules: false
+    react()],
+    build: {
+    rollupOptions: {
+      external: [
+        'react-icons/md',
+        'react-icons/im',
+         'react-icons/fa',
+        'react-hook-form'
+
+      ]
+    }
+  },
+   optimizeDeps: {
+    include: ['react-icons/md', 'react-icons/im']
+  },
+   css: {
+    postcss: './postcss.config.js', // If using PostCSS
+    modules: false // Disable if not using CSS modules
   }
-})
+});
