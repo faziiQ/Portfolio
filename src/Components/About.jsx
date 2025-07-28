54597f37c1aa5dedc95cd98ddee5f57dc37afe89
faziiQ <fm013361@gmail.com>
2025-07-28 23:22:11 +0500

    changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import resume from '../assets/resume.pdf'; // Assuming you have a resume.pdf in assets
 
 const About = () => {
   const handleDownloadResume = () => {
     const link = document.createElement('a');
-    link.href = 'D:\New folder (4)\New folder\Portfolio\src\assets\resume.pdf'; // Path relative to public folder
-    link.download = 'Faizan-Ali-CV.pdf'; // Better filename without special characters
-    link.click();
+    link.href = resume;
+    link.download = 'Faizan-Ali-Resume.pdf';
+    link.click()
 
   };
 
